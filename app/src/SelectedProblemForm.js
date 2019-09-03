858b147d3e828e4bf48f0e5c99c15805ac88ad76
AndyWatt83 <andy.watt83@gmail.com>
2019-09-03 23:29:02 +0100

    Release candidate (#22)
    Some minor tidy ups

--- app/src/SelectedProblemForm.js
+++ app/src/SelectedProblemForm.js
@@ -151,6 +151,31 @@ class SelectedProblemForm extends Component{
         border: '5px solid white'
     };
 
+    renderState = (stateId) =>{
+        console.log(stateId);
+        var stateName = "";
+        switch(stateId) {
+            case "0":
+                stateName = "Initialised";
+                break;
+            case "1":
+                stateName = "Open";
+                break;
+            case "2":
+                stateName = "Solved";
+                break;
+            case "3":
+                stateName = "Complete";
+                break;
+            case "4":
+                stateName = "Cancelled";
+                break;
+            default:
+                stateName = "Unknown"
+        }
+        return <p>{stateName}</p>
+    };
+
     SendIpfsHashToBlockchain = () => {
         return (
             <Container>
@@ -160,7 +185,7 @@ class SelectedProblemForm extends Component{
                     </Col>
                     <Col>
                     <Button className='btn btn-primary btn-block' onClick={this.SendIpfsHashToBlockchainClick}>
-                        Send IPFS
+                        Send Hash to Contract
                     </Button>
                     </Col>
                 </Row>
@@ -184,6 +209,17 @@ class SelectedProblemForm extends Component{
                         method="openProblem"
                         submitText="Open"
                         buttonClassName = "btn btn-primary btn-block"/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        Problem State
+                    </Col>
+                    <Col>
+                        <ContractData
+                            contract="Problem"
+                            method="currentState"
+                            render={this.renderState}/>
                     </Col>
                 </Row>
                 <Row style={this.divStyle}>
